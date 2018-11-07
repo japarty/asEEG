b76e58f8e4555662d6af08db986c87508a787ca0
nekrodezynfekator <marcinjukiewicz@gmail.com>
2018-11-07 21:48:19 +0100

    fix problem with import pyseeg data

--- aseegg.py
+++ aseegg.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# wersja/version 1.9.4
+# wersja/version 1.9.5
 
 """Prosty modul do nauki filtrowania oraz transformowania sygnalu.
 Simple module for signal filtering and fast Fourier transform.
@@ -292,14 +292,19 @@ def formatujPlik(sciezka):
         dane = plikWejsciowy.read().splitlines(True)
 
     samplingRate=dane[2][15:18]
-    daneTemp=[linia.replace(",",", ") for linia in dane if not linia[0]=="%"]
+    daneTemp=[linia for linia in dane if not linia[0]=="%"]
     if samplingRate=='200':
         daneTemp.insert(0, "lp, e1, e2, e3, e4, trigger, a2, a3, time\n")
+        with open(nazwapliku, 'w') as plikWyjsciowy:
+            [plikWyjsciowy.writelines(linia.replace(',', '.').replace('. ', ', '))
+             for linia in daneTemp]
     elif samplingRate=='250':
         daneTemp.insert(0, "lp, e1, e2, e3, e4, e5, e6, e7, e8, a1, a2, a3\n")
-    else:
-        daneTemp.insert(0, "lp, e1, e2, e3, e4, trigger\n")
-
-    with open(nazwapliku, 'w') as plikWyjsciowy:
-        [plikWyjsciowy.writelines(linia.replace(',', '.').replace('. ', ', '))
-         for linia in daneTemp]
+        with open(nazwapliku, 'w') as plikWyjsciowy:
+            [plikWyjsciowy.writelines(linia.replace(',', '.').replace('. ', ', '))
+             for linia in daneTemp]
+    elif dane[0][0:4]=="0,0.":
+        daneTemp.insert(0, "lp,e1,e2,e3,e4,trigger\n")
+        with open(nazwapliku, 'w') as plikWyjsciowy:
+            [plikWyjsciowy.writelines(linia.replace(',', ', '))
+             for linia in daneTemp]
